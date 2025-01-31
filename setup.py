f83699f9c8d91bc7c08ffcadc65a96550eb261e8
ehddnr301 <dy950328@gmail.com>
2025-01-31 05:48:08 +0000

    chore: Rename project from AutoSQL to Lang2SQL

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="autosql",  # 패키지 이름
+    name="lang2sql",  # 패키지 이름
     version="0.1.0",  # 버전
-    description="AutoSQL - Query Generator for Data Warehouse",
+    description="Lang2SQL - Query Generator for Data Warehouse",
     author="ehddnr301",
     packages=find_packages(),  # my_package를 자동으로 찾음
     install_requires=[
@@ -14,11 +14,12 @@ setup(
         "langchain-community==0.3.14",
         "openai==1.59.8",
         "langchain-openai==0.3.0",
+        "streamlit==1.41.1",
     ],
     entry_points={
         "console_scripts": [
             # "my-project" 명령어로 my_package.main 모듈의 run 함수를 실행
-            "autosql = cli.__init__:cli"
+            "lang2sql = cli.__init__:cli"
         ]
     },
 )
